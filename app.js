6d2179cfe2cb26790b15250edef9b812081a5e95
Than Htike Aung <phoehtet25@gmail.com>
2020-09-03 23:36:28 +0630

    socket room

--- app.js
+++ app.js
@@ -15,14 +15,29 @@ let express = require('express'),
          res.render('index');
       })
 
+      let userMap =new Map();
+      let room1 = "public";
+      let room2 = "private";
+
       io.sockets.on('connection', (socket)=>{
          socket.on("login", data =>{
             socket.username = data;
-            io.sockets.connected[socket.id].emit('login-success', true);
+            userMap.set(socket.username, socket.id)
+            if(data == 'w' || data == 'x'){
+               socket.join(room1);
+               socket.userroom = room1;
+            }else{
+               socket.join(room2);
+               socket.userroom = room2;
+            }
+            socket.emit('login-success', true);
+
+           // io.sockets.connected[socket.id].emit('login-success', true);
            // io.emit('login-success', true);
          });
          socket.on('msg', data=>{
-            io.emit('income-msg', socket.username + " : " + data);
+            io.in(socket.userroom).emit('income-msg', socket.username + ": " + data);
+           // io.emit('income-msg', socket.username + " : " + data);
          })
       })
 
